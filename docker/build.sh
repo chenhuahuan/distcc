590a7718235e3e753c4cc93086a068bba5011b81
kent.chen <kent.chen@dji.com>
2019-12-17 21:40:11 +0800

    update, apt-get

--- docker/build.sh
+++ docker/build.sh
@@ -14,14 +14,14 @@ fi
 for compiler in "${compilers[@]}"
 do
   echo "*** Building distcc/$compiler image"
-  docker build -t distcc/$compiler -f compilers/Dockerfile.$compiler .
+  docker build -t distcc/gcc-5-33 -f compilers/Dockerfile.$compiler ..
 done
 
-echo "*** Building distcc"
-for compiler in "${compilers[@]}"
-do
-  echo "*** Building distcc with distcc/$compiler image"
-  set -x
-  docker run --rm -it -v /tmp:/tmp -v `pwd`/..:/src:rw -w /src distcc/$compiler bash -c "./autogen.sh && ./configure && make clean && make && make install && make check" &> distcc-$compiler.log
-  set +x
-done
+#echo "*** Building distcc"
+#for compiler in "${compilers[@]}"
+#do
+#  echo "*** Building distcc with distcc/$compiler image"
+#  set -x
+#  docker run --rm -it -v /tmp:/tmp -v `pwd`/..:/src:rw -w /src distcc/$compiler bash -c "./autogen.sh && ./configure && make clean && make && make install && make check" &> distcc-$compiler.log
+#  set +x
+#done
